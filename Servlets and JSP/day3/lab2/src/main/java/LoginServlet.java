078e7d7e76a42a7878a32375ff625af64b1b7a49
MostafaYahia <mostafayehyax23@gmail.com>
2021-03-06 07:25:49 +0200

    Complete lab 2 day 3

--- Servlets and JSP/day3/lab2/src/main/java/LoginServlet.java
+++ Servlets and JSP/day3/lab2/src/main/java/LoginServlet.java
@@ -1,71 +1,109 @@
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.naming.NamingException;
+import java.sql.PreparedStatement;
+import javax.sql.DataSource;
+import java.sql.ResultSet;
 
 import java.io.*;
-import java.util.*;  
+import java.util.*;
 
-public class LoginServlet extends HttpServlet{
+public class LoginServlet extends HttpServlet {
 
     ServletConfig servletConfig;
-    
-    public void init(ServletConfig config) throws ServletException{
+
+    public void init(ServletConfig config) throws ServletException {
 
         servletConfig = config;
-}
+    }
 
-public void doGet(HttpServletRequest request,
-HttpServletResponse response)
-throws ServletException, IOException
-{
-  response.setContentType("text/html");
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
 
-  PrintWriter out = response.getWriter();
-  String loginStatus = request.getParameter("loginStatus");
+        PrintWriter out = response.getWriter();
+        String loginStatus = request.getParameter("loginStatus");
+        String registrationStatus = request.getParameter("registerStatus");
 
-     if(loginStatus !=null &&loginStatus.equals("fail")){
+        out.println("<FORM method= POST>");
+        out.println("<BR> Username: <INPUT TYPE=TEXT NAME=userName>");
+        out.println("<BR> Password: <INPUT TYPE=PASSWORD NAME=password>");
+        out.println("<BR> <INPUT TYPE=SUBMIT VALUE=Submit>");
+        out.println("</FORM>");
 
-              out.println("<FORM method= POST>");
-              out.println("<BR> Username: <INPUT TYPE=TEXT NAME=userName>");
-              out.println("<BR> Password: <INPUT TYPE=PASSWORD NAME=password>");
-              out.println("<BR> <INPUT TYPE=SUBMIT VALUE=Submit>");
-              out.println("</FORM>");
-              out.println("<p style='color:red'>Login Failed, please tryagain<p>");
-     }else{
+        if (loginStatus != null && loginStatus.equals("fail")) {
+            out.println("<p style='color:red'>Login Failed, please tryagain<p>");
+        }
 
-          out.println("<FORM method= POST>");
-          out.println("<BR> Username: <INPUT TYPE=TEXT NAME=userName>");
-          out.println("<BR> Password: <INPUT TYPE=PASSWORD NAME=password>");
-          out.println("<BR> <INPUT TYPE=SUBMIT VALUE=Submit>");
-          out.println("</FORM>");
-     }
+        if(registrationStatus !=null && registrationStatus.equals("success")){
+            out.println("<p style='color:green'>Registration success, please login<p>");
 
-}
+        }
 
-public void doPost(HttpServletRequest request,
-HttpServletResponse response)
-throws ServletException, IOException{
+    }
+
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response)
+            throws ServletException, IOException {
 
 
-       RequestDispatcher requestDispatcher = request.getRequestDispatcher("DisplayParametersServlet");
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("DisplayParametersServlet");
 
-            response.setContentType("text/html");
-            PrintWriter out = response.getWriter();
-    
-            String userName = request.getParameter("userName");
-            String password = request.getParameter("password");
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
 
-            if(userName.equals("mostafa") && password.equals("123456")) {
-          
-              response.getWriter().println("<p>Hello from login Servlet</p>");
-              requestDispatcher.forward(request,response);
-            
+        String userName = request.getParameter("userName");
+        String password = request.getParameter("password");
+
+        // I should connect here to DB and check against the data
+
+        String resultUserName = "";
+        String resultPassword = "";
+        try {
+            Context initContext = new InitialContext();
+            Context envContext = (Context) initContext.lookup("java:/comp/env");
+            DataSource ds = (DataSource) envContext.lookup("jdbc/TestDB");
+            Connection conn = ds.getConnection();
+
+            PreparedStatement preparedStatement = conn.prepareStatement("select * from my_users where user_name = ? and password = ?");
+
+            preparedStatement.setString(1, userName);
+            preparedStatement.setString(2, password);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                resultUserName = resultSet.getString(4);
+                resultPassword = resultSet.getString(5);
+
+                preparedStatement.close();
             }
-            else { 
-              response.sendRedirect("LoginServlet?loginStatus=fail");
-            }
 
-}
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+
+
+        if (userName.equals(resultUserName) && password.equals(resultPassword)) {
+
+            requestDispatcher.forward(request, response);
+
+        } else {
+            response.sendRedirect("LoginServlet?loginStatus=fail");
+        }
+
+    }
 
 
 }
---
+++ Servlets and JSP/day3/lab2/src/main/java/RegisterServlet.java
@@ -0,0 +1,114 @@
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.naming.NamingException;
+import java.sql.PreparedStatement;
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+
+import java.io.*;
+import java.util.*;
+
+public class RegisterServlet extends HttpServlet {
+
+    ServletConfig servletConfig;
+
+    public void init(ServletConfig config) throws ServletException {
+
+        servletConfig = config;
+    }
+
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        String registerStatus = request.getParameter("registerStatus");
+
+        out.println("<FORM style='margin: 0 auto;' method= POST>");
+        out.println("<h1 style='font-size:300%; font-weight: bold;'>Registration Form</h1>");
+        out.println("<h2>Please Enter the user details</h2>");
+
+        out.println("<BR> FirstName: <INPUT TYPE=TEXT NAME=firstName>");
+        out.println("<BR><BR> LastName: <INPUT TYPE=TEXT NAME=lastName>");
+        out.println("<BR><BR> UserName: <INPUT TYPE=TEXT NAME=userName>");
+        out.println("<BR><BR> Password: <INPUT TYPE=PASSWORD NAME=password>");
+
+        out.println("<BR><BR> <INPUT TYPE=RESET VALUE=Reset>");
+        out.println("<INPUT TYPE=SUBMIT VALUE=Submit button>");
+        out.println("</FORM>");
+
+
+        if (registerStatus != null && registerStatus.equals("fail")) {
+            out.println("<p style='color:red'>Register Failed, please try again<p>");
+        }
+
+    }
+
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response)
+            throws ServletException, IOException {
+
+
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("LoginServlet");
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+
+        String userName = request.getParameter("userName");
+        String password = request.getParameter("password");
+        int rowsEffected = 0;
+
+        if (firstName== null || firstName.isEmpty() ||lastName== null || lastName.isEmpty() ||
+                password== null || password.isEmpty() ||userName== null || userName.isEmpty()) {
+            response.sendRedirect("RegisterServlet?registerStatus=fail");
+            return;
+        }
+
+        try {
+            Context initContext = new InitialContext();
+            Context envContext = (Context) initContext.lookup("java:/comp/env");
+            DataSource ds = (DataSource) envContext.lookup("jdbc/TestDB");
+            Connection conn = ds.getConnection();
+
+            PreparedStatement preparedStatement = conn.prepareStatement("insert into my_users(first_name,last_name,user_name,password)  values(?,?,?,?)");
+
+            preparedStatement.setString(1, firstName);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setString(3, userName);
+            preparedStatement.setString(4, password);
+
+            rowsEffected = preparedStatement.executeUpdate();
+
+            preparedStatement.close();
+            conn.close();
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+
+
+        if (rowsEffected == 0 || firstName== null || firstName.isEmpty() ||lastName== null || lastName.isEmpty() ||
+        password== null || password.isEmpty() ||userName== null || userName.isEmpty()) {
+            response.sendRedirect("RegisterServlet?registerStatus=fail");
+        } else {
+            response.sendRedirect("LoginServlet?registerStatus=success");
+        }
+
+    }
+
+}
