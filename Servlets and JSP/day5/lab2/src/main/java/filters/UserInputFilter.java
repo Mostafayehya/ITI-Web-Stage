eb7885bed2af5f9d9543662212c9e2859398e109
MostafaYahia <mostafayehyax23@gmail.com>
2021-03-14 11:44:14 +0200

    Finish Selvets and JSP course labs

---
+++ Servlets and JSP/day5/lab2/src/main/java/filters/UserInputFilter.java
@@ -0,0 +1,2 @@
+package filters;public class UserInputFilter {
+}
---
+++ Servlets and JSP/day7/lab2/src/main/java/filters/IPLoggerFilter.java
@@ -0,0 +1,30 @@
+package filters;
+
+import jakarta.servlet.*;
+
+import java.io.IOException;
+
+public class IPLoggerFilter implements Filter {
+
+    private FilterConfig filterConfig;
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+        this.filterConfig = filterConfig;
+        filterConfig.getServletContext().log("Filter initialized");
+    }
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        filterConfig.getServletContext().log("+++++++++++++++++++++++++++++++++++ IP of the host+++++++++++++ \n "+ servletRequest.getRemoteAddr());
+
+
+    }
+
+    public void destroy() {
+
+        filterConfig.getServletContext().log("Filter destroyed");
+        this.filterConfig = null;
+
+    }
+}
